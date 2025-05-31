374947db9b4caa40c6388019c756a287cff25c8f
GarvitSingh <garvits893@gmail.com>
2025-05-31 12:09:45 +0530

    basic program to add a b

---
+++ basics/sumofAB.java
@@ -0,0 +1,12 @@
+
+
+public class sumofAB {
+
+    public static void main(String args[]){
+        byte a = 10;
+        byte b = 5;
+        int sum = a+b;
+        System.out.println(sum);  
+    }
+    
+}
