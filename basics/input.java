7cc1cd0a35a8ad3005c04e8a6e12173bccb5f19e
GarvitSingh <garvits893@gmail.com>
2025-05-31 12:48:31 +0530

    take input using scanner

---
+++ basics/input.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+// import scanner used to take input 
+
+class input{
+    public static void main(String args[]){
+
+        // make new object of scanner
+        Scanner sc =new Scanner(System.in);
+        
+        // calling next function of scanner
+        String input = sc.next();
+         
+        System.out.println(input);
+    }
+}
