31aa870d5de5eff1458aec0207fa8c03fe8c7700
GarvitSingh <garvits893@gmail.com>
2025-06-14 09:12:55 +0530

    output in java

---
+++ basics/Sout.java
@@ -0,0 +1,9 @@
+public class Sout {
+    public static void main(String[] args) {
+       System.out.println(2+"2"+2); // 222
+        
+        System.out.println(2+2+"2"); //4 +"2"  42
+        
+        System.out.println(2+2+"2"+2+2); //4 + "2" +2 +2 = "42" +2 +2 = "422" +2 =  4222
+    }
+}
