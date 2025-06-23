003583f716dea3573d429e4019796a359b551a79
GarvitSingh <garvits893@gmail.com>
2025-06-23 17:26:22 +0530

    program to get the result using ternary operator

---
+++ conditionals/passFail.java
@@ -0,0 +1,12 @@
+// using ternary operator 
+public class passFail {
+
+    public static void main(String[] args){
+        int marks = 45 ;
+
+        String result = (marks > 30 ) ? "pass" : "fail";
+
+        System.out.println(result);
+    }
+    
+}
