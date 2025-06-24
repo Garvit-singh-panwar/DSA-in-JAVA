27513f1974895dda8a4feb6a746929226d31ea13
GarvitSingh <garvits893@gmail.com>
2025-06-24 16:41:01 +0530

    make a program to print value 1 to 10 using loops

---
+++ loops/Print1To10.java
@@ -0,0 +1,13 @@
+public class Print1To10 {
+// Print num from 1 to 10 
+    public static void main(String[] args) {
+
+        int num = 1;
+
+        while (num <=10){
+            System.out.print(num + " ");
+            num++;
+        }
+        
+    }
+}
