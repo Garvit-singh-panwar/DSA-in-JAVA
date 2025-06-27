9547b22c6eb5b65c3985f13b7e803b80ddcfb13a
GarvitSingh <garvits893@gmail.com>
2025-06-27 15:17:51 +0530

    product of a and b

---
+++ function/product.java
@@ -0,0 +1,18 @@
+public class product {
+
+    public static int prod(int a , int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+
+       int prod =  prod(a,b);
+
+       System.out.println( "Product of a and b = " + prod);
+
+
+    }
+
+}
