671760952ed74987e51a816527ed25dc9ba33486
GarvitSingh <garvits893@gmail.com>
2025-07-16 16:35:18 +0530

    information about call stack

---
+++ recursion/StackOverFlow.java
@@ -0,0 +1,9 @@
+// When we call a function, it occupies space in the call stack.
+
+// All variables and data related to the function are stored in memory.
+
+// If too many function calls are made (especially in recursion), the stack memory can get full.
+
+// This situation is called a **stack overflow**, and it leads to a runtime error.
+
+// In such cases, the stack can become dangerous for the program’s stability.
