a14e2234798d13a903a8aa38f7823a431d8d20aa
GarvitSingh <garvits893@gmail.com>
2025-07-25 18:48:17 +0530

    print substrings using backtracking

---
+++ BackTracking/TwoChoice.java
@@ -0,0 +1,41 @@
+package BackTracking;
+
+public class TwoChoice {
+
+// This function prints all the subsequences (substrings formed by skipping or including characters)
+// of a given string using recursion. It takes three arguments:
+// - str: the original input string
+// - ans: the current subsequence being built
+// - i: the current index in the original string
+
+// The idea is based on recursive choices:
+// At each character in the string, we have two options:
+// 1. Include the character in the current 'ans' string.
+// 2. Exclude the character and move to the next one.
+
+// This process continues recursively for all characters until we reach the base case
+// (i == str.length()), at which point we print the current 'ans'.
+
+
+    public static void subString(String str , String ans , int i){
+        // Base case : if we have reached at the end of the string 
+        if(i >= str.length()){
+            System.out.println(ans + " ");
+            return ;
+        }
+
+        subString(str, ans+str.charAt(i), i+1); // yes choice: include the character
+        subString(str, ans, i+1); // no choice: exclude the character 
+
+    }
+
+    public static void main(String[] args){
+        String str = "abc";
+        String ans ="";
+
+        subString(str, ans, 0);
+
+    }
+    
+}
+
